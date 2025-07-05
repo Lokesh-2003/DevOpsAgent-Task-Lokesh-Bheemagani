228f15ec31b7f6567f1d1728eb990cc6d50d92da
Lokesh Bheemagani <142565652+Lokesh-2003@users.noreply.github.com>
2025-07-05 19:31:42 +0530

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -1,1 +1,13 @@
+from prometheus_api_client import PrometheusConnect
+import datetime
 
+prom = PrometheusConnect(url="http://localhost:9090", disable_ssl=True)
+
+def check_cpu_spike(threshold=80.0):
+    query = '100 - (avg by(instance)(irate(node_cpu_seconds_total{mode="idle"}[2m])) * 100)'
+    result = prom.custom_query(query)
+    if result:
+        value = float(result[0]['value'][1])
+        if value > threshold:
+            return True, value
+    return False, 0.0
